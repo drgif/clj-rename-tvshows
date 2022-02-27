47dce308656c5f058c3d06cd22d7d015cff23282
Michael Wiederhold <michael.wiederhold@luminari.de>
2022-02-27 21:20:07 +0100

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-rename-tvshows
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clj_rename_tvshows/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-rename-tvshows.core-test
+  (:require [clojure.test :refer :all]
+            [clj-rename-tvshows.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))

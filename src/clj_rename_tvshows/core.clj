87e815d14e7415d58ada716051c184b26c58c176
Michael Wiederhold <michael.wiederhold@luminari.de>
2022-02-27 21:46:27 +0100

    Some tests for listing files

--- src/clj_rename_tvshows/core.clj
+++ src/clj_rename_tvshows/core.clj
@@ -1,7 +1,19 @@
 (ns clj-rename-tvshows.core
+  (:require [clojure.java.io])
   (:gen-class))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(defn- run
+  "All the logic in one function"
+  [basefolder]
+  (->> basefolder
+       (clojure.java.io/file)
+       (file-seq)
+       (map #(.getName %))))
+
+(comment
+  (run "./doc"))
